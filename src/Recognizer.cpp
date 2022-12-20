fcd8404c0d54b229e079f182484a62173b5084aa
Zolboo Erdenebayar <zolboo.erdenebayar@tomra.com>
2022-12-20 11:46:32 +0100

    4.1. 2. Linear search

--- src/Recognizer.cpp
+++ src/Recognizer.cpp
@@ -19,6 +19,23 @@
 
 namespace {
     static const double INF = std::numeric_limits<double>::infinity();//define the positiv infinity of Double in the namespace
+    template <class T>
+    void printv(const std::vector<T>& w) {
+        for (const auto& a : w) {
+          std::cout << a << " ";
+        }
+        std::cout << std::endl;
+    };
+    template<class T>
+    void printdv(std::vector<std::vector<T>> v) {
+        for (auto& a : v) {
+          for (auto &b: a) {
+            std::cout << b << " ";
+          }
+          std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    };
 }
 
 /*****************************************************************************/
@@ -73,11 +90,75 @@ void Recognizer::recognize(Corpus const& corpus) {
 /*****************************************************************************/
 
 void Recognizer::recognizeSequence(FeatureIter feature_begin, FeatureIter feature_end, std::vector<WordIdx>& output) {
-  // TODO: implement
+  scorer_.prepare_sequence(feature_begin, feature_end);
+  size_t T = feature_end - feature_begin;
+  std::vector<std::vector<double>> cur;
+  std::vector<std::vector<double>> prev;
+  auto resize = [&](std::vector<std::vector<double>>& v) {
+    v.resize(lexicon_->num_words());
+    for (WordIdx w = 0; w < lexicon_->num_words(); w++) {
+      v.at(w).resize(lexicon_->get_automaton_for_word(w).num_states() + 1);
+      std::fill(v.at(w).begin() + 1, v.at(w).end(), INF);
+    }
+  };
+  resize(cur);
+  resize(prev);
+  prev.front().back() = 0;
+  std::vector<std::vector<int>> B(lexicon_->num_words());
+  size_t B_width = T + 1;
+  for (WordIdx w = 0; w < lexicon_->num_words(); w++)
+    B.at(w).resize(B_width * (lexicon_->get_automaton_for_word(w).num_states() + 1));
+  std::vector<int> B1(T + 1);
+
+  std::vector<WordIdx> W(T + 1);
+  auto d = [&](size_t t, size_t s, WordIdx& w) -> double {
+      auto feat = feature_begin + t - 1;
+      auto state = lexicon_->get_automaton_for_word(w)[s - 1];
+      return scorer_.score(feat, state);
+  };
+
+  auto S = [&](const WordIdx& w) -> size_t { return lexicon_->get_automaton_for_word(w).num_states(); };
   // scorer_.prepare_sequence(feature_begin, feature_end);
-  // for(auto feat = feature_begin; feat < feature_end; feat++) {
-  //   scorer_.score(feat);
-  // }
+  for(size_t t = 1; t <= T; t++) {
+    for (WordIdx w = 0; w < lexicon_->num_words(); w++) {
+      prev.at(w).at(0) = prev.at(W.at(t-1)).at(S(W.at(t-1))) + word_penalty_;
+      B.at(w).at(t - 1 + 0 * B_width) = t - 1;
+      size_t s = 1;
+      for (; s <= S(w); s++) {
+        std::vector<double> to_scores;
+        for (size_t i = 0; i <= std::min(2ul, s); i++)
+          to_scores.emplace_back(prev.at(w).at(s - i) + tdp_model_.score(lexicon_->get_automaton_for_word(w)[s - 1], i));
+        auto min = std::min_element(to_scores.begin(), to_scores.end());
+        size_t argmin = min - to_scores.begin();
+        if (*min == INF) {
+          cur.at(w).at(s) = INF;
+          continue;
+        }
+        cur.at(w).at(s) = d(t, s, w) + *min;
+        B.at(w).at(t + s * T) = B.at(w).at(t - 1 + T * (s - argmin));
+      }
+    }
+    WordIdx w_min = 0;
+    double min_score = INF;
+    for (WordIdx w = 0; w < lexicon_->num_words(); w++) {
+      if (cur.at(w).back() < min_score) {
+        min_score = cur.at(w).back();
+        w_min = w;
+      }
+    }
+    B1.at(t) = B.at(w_min).at(t + T * S(w_min));
+    W.at(t) = w_min;
+    std::swap(prev, cur);
+  }
+  size_t n = 0;
+  size_t t = T;
+  output.clear();
+  while (t > 0) {
+    n++;
+    if (W.at(t) != lexicon_->silence_idx())
+      output.insert(output.cbegin(), W.at(t));
+    t = B1.at(t);
+  }
 }
 
 /*****************************************************************************/
